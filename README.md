a8c36e6cea950cce1d50f7a1e73d3921d31a7808
Ahmed Al-Saey <16p6000@eng.asu.edu.eg>
2018-07-03 00:34:33 +0200

    Commenting and syntax improvement

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 # Task-1---Front-end-workshop
-Submitor's name: Ahmed Hesham Mohamed AlSaey</br>
-ID: 16P6000
--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-var list = document.querySelector('#list-ui');
-var addTextField = document.querySelector('#add-textfield')
-var searchTextField = document.querySelector('#search-textfield');
-var addBtn = document.querySelector('#add-button');
-var itemsHTMLStrings = [];
-var itemsStrings = [];
-var alerts = [];
-var alertsContainer = document.querySelector('#alerts-container');
+var list = document.querySelector('#list-ui'); //Main unordered list
+var addTextField = document.querySelector('#add-text-field') //Text field for adding items
+var searchTextField = document.querySelector('#search-text-field'); //Text field for searching items
+var addBtn = document.querySelector('#add-button'); //Add item button
+var itemsHTMLStrings = []; //Array containing all HTML text for list items
+var itemsStrings = []; //Array containing all text for list items
+var alerts = []; //Array containing all triggered alerts (EXTRA FEATURE)
+var alertsContainer = document.querySelector('#alerts-container'); //div containing the alerts (EXTRA FEATURE)
 
 //-----------------------------------------
 
 addBtn.addEventListener('click', addItem);
-
+//Adds HTML content of the item taked from input field to the main array containing all HTML content of all items (itemsHTMLStrings), and adds item text to 
+//the main array containing all text content of all items (itemsStrings, which is used for substring search in function "searchList" below).
+//Then, refreshes the list by calling "refresh" function and passing the array with the full list of item's HTML content (see refresh below)
 function addItem(event) {
     itemsHTMLStrings.push(`<li class='list-items list-group-item'>${addTextField.value}</li>`);
     itemsStrings.push(addTextField.value);
@@ -21,6 +23,8 @@ function addItem(event) {
 
 searchTextField.addEventListener('input', searchList);
 
+//Creates a temporary array containing strings of HTML content of items that match substring search and sends that temporary array to refresh function.
+//(See "refresh" below)
 function searchList(event) {
     var temp = [];
     for (var i = 0; i < itemsStrings.length; i++) {
@@ -33,6 +37,7 @@ function searchList(event) {
 
 //-----------------------------------------
 
+//Refreshes the main unordered list by taking an array of strings (inputList) containing HTML content of each list item and viewing it in the UI.
 function refreshUI(inputList) {
     if (inputList.length == 0) {
         list.innerHTML = "";
@@ -49,6 +54,9 @@ function refreshUI(inputList) {
 
 //-----------------------------------------
 
+//EXTRA FEATURE:
+//Shows "No items found" alert for 3 seconds when no items are found on a key stroke, it lasts 3 seconds from the last key stroke pressed that triggered it. 
+//(there could be multiple consecutive key strokes that triggered it)
 function showNoItemsAlert() {
 
     alerts.push('<div class="alert alert-danger" id="not-found-alert" role="alert">No items found</div>');
